611836043e35416ad1957d7aac924362e0bc90ed
Héctor Aguado <haguadop@gmail.com>
2018-02-06 19:03:42 +0100

    modifico lib/connectMongoose.js para que se pueda conectar con usuario y contraseña a la base de datos nodepopdb

--- nodepop/lib/connectMongoose.js
+++ nodepop/lib/connectMongoose.js
@@ -3,6 +3,13 @@
 const mongoose = require('mongoose');
 const conn = mongoose.connection;
 
+var databaseUri = 'mongodb://$localhost/nodepopdb'
+if (process.env.DB_USER){
+    databaseUri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost/nodepopdb`;
+    console.log('USUARIO:',process.env.DB_USER);
+    console.log('PASS:',process.env.DB_PASS);
+}
+
 conn.on('error', err => {
     console.log('VAYA, HA HABIDO UN ERROR CONECTANDO A LA BBDD CON MONGOOSE', err);
     process.exit(1);
@@ -12,7 +19,7 @@ conn.once('open', () => {
     console.log(`Conectado a mongoDB con Mongoose en ${mongoose.connection.name}`)
 });
 
-mongoose.connect('mongodb://localhost/nodepopdb',{
+mongoose.connect(databaseUri,{
     useMongoClient: true
 });
 
